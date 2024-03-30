e599f71499565bdf5773c4df50ad099764d76d28
erik.minasian <erik.minasian@coliquio.de>
2024-03-30 13:25:30 +0400

    Explanation for vanilla CSS scoping

--- src/routes/VanillaCSSNotScoping/VanillaCSSNotScoping.jsx
+++ src/routes/VanillaCSSNotScoping/VanillaCSSNotScoping.jsx
@@ -11,7 +11,7 @@ export default function VanillaCSSNotScoping() {
         <Component1 />
         <Component2 />
       </DisplaySection>
-      {/* <ExplanationSection explanation={explanation} /> */}
+      <ExplanationSection explanation={explanation} />
     </>
   );
 }
